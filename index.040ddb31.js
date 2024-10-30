e2f1b0f8e04b22c6889cf076d6b56122f3c852f8
Igor <igor.olshanskyi@gmail.com>
2024-10-30 20:05:34 +0200

    make build

---
+++ index.040ddb31.js
@@ -0,0 +1,2 @@
+var e=document.querySelectorAll("tr"),t=!0,r=!1,l=void 0;try{for(var n,a=e[Symbol.iterator]();!(t=(n=a.next()).done);t=!0){var o=n.value,i=document.createElement("item.tagName");i=o.children[1].cloneNode(!0),o.lastElementChild.insertAdjacentElement("beforebegin",i)}}catch(e){r=!0,l=e}finally{try{t||null==a.return||a.return()}finally{if(r)throw l}}
+//# sourceMappingURL=index.040ddb31.js.map
